4543f5f6b17940c7498cdb163acb11094a1a6287
grumbleBee <s.kyne96@gmail.com>
2016-09-28 19:43:34 -0400

    Potentially-functional Game Controller
    (-_-)b

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager instance = null;
+
+	// make sure the constructor is private, so it can only be instantiated here
+	private GameManager() {
+	}
+
+	//buncha variables (hmu with more variables)
+	private int variable;
+
+	//set & get methods for all those variables
+	public void setVariable(int newVar){
+		variable = newVar;
+	}
+
+	public int getVariable(){
+		return variable;
+	}
+		
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			//enforces singleton pattern
+			Destroy (gameObject);
+		}
+			
+		DontDestroyOnLoad (gameObject);
+	}
+}
