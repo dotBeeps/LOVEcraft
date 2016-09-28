41ca70bc117daa6f19c3e08b0076d44d2cb606ae
Dave <ddogclaw@gmail.com>
2016-09-28 18:46:29 -0400

    RoomGen
    Generates rooms

---
+++ Assets/GenerateMap.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateMap : MonoBehaviour {
+
+    private int roomsGenerated = 0;
+    private List<GameObject> openRooms = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        GenerateRooms(20);
+	}
+	
+    void GenerateRooms(int numRooms)
+    {
+        openRooms.Add(Instantiate(Resources.Load("Rooms/RoomBase") as GameObject,Vector3.zero,Quaternion.identity) as GameObject);
+        roomsGenerated++;
+
+        while (roomsGenerated-2<numRooms)
+        {
+            GameObject curRoom = openRooms[Random.Range(0, Mathf.FloorToInt(openRooms.Count - 1))];
+            RoomStart roomScript = curRoom.GetComponent<RoomStart>();
+            int dir = Mathf.RoundToInt(Random.Range(0, 3));
+            //Debug.Log("NUMBER IS");
+            //Debug.Log(dir);
+            Debug.Log("START GEN NEW ROOM");
+            while (!roomScript.doors[dir])
+            {
+                Debug.Log("Fixing Direction");
+                if (dir < 4)
+                    dir++;
+                else
+                    dir = 0;
+            }
+            float yOffset = 0;
+            float xOffset = 0;
+            if (dir % 2 == 0)
+                yOffset = 11;
+            else
+                xOffset = 19;
+            if (dir>1)
+            {
+                yOffset *= -1;
+                xOffset *= -1;
+            }
+
+            Vector3 offset = new Vector3(xOffset, yOffset,0);
+            GameObject newRoom = Instantiate(Resources.Load("Rooms/RoomBase") as GameObject, curRoom.transform.position + offset, Quaternion.identity) as GameObject;
+            roomScript.doors[dir] = false;
+            RoomStart newScript = newRoom.GetComponent<RoomStart>();
+            if (dir > 1)
+                newScript.doors[dir - 2] = false;
+            else
+                newScript.doors[dir + 2] = false;
+
+            openRooms.Add(newRoom);
+            roomScript.neighbors.Add(newRoom);
+            if (roomScript.neighbors.Count >= 4)
+                openRooms.Remove(curRoom);
+            roomsGenerated++;
+        }
+
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/RoomStart.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomStart : MonoBehaviour {
+
+    public bool northDoor = true;
+    public bool southDoor = true;
+    public bool eastDoor = true;
+    public bool westDoor = true;
+
+    public GameObject northRoom;
+    public GameObject southRoom;
+    public GameObject eastRoom;
+    public GameObject westRoom;
+
+    public bool[] doors = new bool[4];
+    public ArrayList neighbors = new ArrayList();
+
+	// Use this for initialization
+	void Awake () {
+        doors[0] = northDoor;
+        doors[1] = eastDoor;
+        doors[2] = southDoor;
+        doors[3] = westDoor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
