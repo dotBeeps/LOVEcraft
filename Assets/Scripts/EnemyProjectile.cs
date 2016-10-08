a45310a0780c99d18a5cf4fef64ad7146248c089
grumbleBee <s.kyne96@gmail.com>
2016-10-08 19:05:26 -0400

    Enemies Can Shoot and You Can Die! Maybe
    Yaaaaaaay

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProjectile : MonoBehaviour {
+
+	float speed; //the bullet speed
+	Vector2 _direction; //the direction of the bullet
+	bool isReady; //whether the bullet direction is set
+
+	//set default value in Awake function
+	void Awake(){
+		speed = 5f;
+		isReady = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetDirection(Vector2 direction){
+		//set the direction normalized to get a unit vector
+		_direction = direction.normalized;
+
+		isReady = true; //set flag to true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isReady) {
+			//get the bullet's current position
+			Vector2 position = transform.position;
+
+			//compute the bullet's new position
+			position += _direction * speed * Time.deltaTime;
+
+			//update the bullet's position;
+			transform.position = position;
+
+			//remove bullet if it goes outside the screen
+			Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0)); //bottom left
+			Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1)); //top right
+
+			//if the bullet leaves the screen, destroy it
+			if ((transform.position.x < min.x) || (transform.position.x > max.x) ||
+			    (transform.position.y < min.y) || (transform.position.y > max.y)) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour{
+	public GameObject EnemyProjectile; //enemy projectile prefab
+	private float delay = 3f;
+	private float timer = 2f;
+
+	//use this for initialization
+	void Start(){
+		InvokeRepeating("FireEnemyProjectile", delay, timer);
+	}
+
+	//update is called once per frame
+	void Update(){
+	}
+
+	//function to fire enemy projectile
+	void FireEnemyProjectile(){
+		//get reference to player
+		GameObject player = GameObject.Find("guy_1");
+
+		if(player != null){ //if player isn't dead
+			//instantiate enemy projectile
+			GameObject projectile = (GameObject)Instantiate(EnemyProjectile);
+
+			//set projectile's initial position
+			projectile.transform.position = transform.position;
+
+			//computer projectile's direction towards player
+			Vector2 direction = player.transform.position - projectile.transform.position;
+
+			//set projectile's direction
+			projectile.GetComponent<EnemyProjectile>().SetDirection(direction);
+		}
+	}
+}
