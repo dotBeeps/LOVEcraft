40c6ed78b25211e1c0a58d96b4b662aff45d9752
Vendeta25 <rhavanas@gmail.com>
2016-10-09 17:04:35 -0400

    ryan tries to do things today

---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour {
+    GameObject player;
+    float MoveSpeed = 5f;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        Vector3 fixedTarget = player.transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, fixedTarget- transform.position);
+        if (Vector3.Distance(transform.position, player.transform.position) >= .5)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, fixedTarget, MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+            
+
+	}
+}
