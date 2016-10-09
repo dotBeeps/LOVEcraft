4cf808fa43680d7cc14404f3e08bbcc5cea6deab
Dave <ddogclaw@gmail.com>
2016-10-09 16:52:49 -0400

    You Look the right way
    Eyyyyy

---
+++ Assets/Scripts/IObtainable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obtainable
+{
+    enum Type { LockedChest, UnlockedChest, OpenChest, Item };
+}
+
+
+public interface IObtainable {
+
+    void setObtainableType(int type);
+
+    void setItemId(int id);
+	
+}
---
+++ Assets/Scripts/Weapons/Ranged/Mixtape.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mixtape : RangedWeapon {
+
+	public Mixtape(int id, string name) : base(id, name)
+    {
+
+    }
+
+    public override void Attack()
+    {
+
+        base.Attack();
+    }
+}
