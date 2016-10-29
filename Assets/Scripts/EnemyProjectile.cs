4ff5661b4d87aa55217f08b249b8a8ce76f00872
grumbleBee <s.kyne96@gmail.com>
2016-10-29 00:58:22 -0400

    Weapon Switching Thing
    idk it works ¯\_(ツ)_/¯

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -48,8 +48,6 @@ public class EnemyProjectile : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col){
-		//Check collision name
-		Debug.Log("collision name = " + col.gameObject.name);
 		if(col.gameObject.name != "enemy"){
 			Destroy(gameObject);
 		}
---
+++ Assets/Scripts/UIWeaponSwap.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class UIWeaponSwap : MonoBehaviour {
+	GameObject[] gameObjectArray;
+
+
+	// Use this for initialization
+	void Start (){
+		gameObjectArray = GameObject.FindGameObjectsWithTag ("WeaponImage");
+		activate (9);
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			activate (9);
+		} else if (Input.GetKey (KeyCode.Alpha2)) {
+			activate (8);
+		} else if (Input.GetKey (KeyCode.Alpha3)) {
+			activate (7);
+		} else if (Input.GetKey (KeyCode.Alpha4)) {
+			activate (6);
+		} else if (Input.GetKey (KeyCode.Alpha5)) {
+			activate (5);
+		} else if (Input.GetKey (KeyCode.Alpha6)) {
+			activate (4);
+		} else if (Input.GetKey (KeyCode.Alpha7)) {
+			activate (3);
+		} else if (Input.GetKey (KeyCode.Alpha8)) {
+			activate (2);
+		} else if (Input.GetKey (KeyCode.Alpha9)) {
+			activate (1);
+		} else if (Input.GetKey (KeyCode.Alpha0)) {
+			activate (0);
+		}
+	
+	}
+
+	void deactivate(){
+		Debug.Log (gameObjectArray);
+		foreach(GameObject go in gameObjectArray)
+		{
+			go.SetActive (false);
+		}
+	}
+
+	void activate(int i){
+		deactivate ();
+		gameObjectArray[i].SetActive(true);
+	}
+}
