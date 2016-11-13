66f942dd4dddd0bd7ca1c94d9a4fbaaa0cfc420a
ANonTakenUsername <Main@Nialls-MacBook-Pro.local>
2016-11-13 12:53:07 -0500

    Changing health from int to float

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,8 +6,8 @@ public class PlayerHealth : MonoBehaviour{
 	private static GameManager gameManager;
 	public static GameObject GameController;
 
-	public int startingHealth;                                  // The amount of health the player starts the game with.
-	public int currentHealth;                                   // The current health the player has.
+	public float startingHealth;                                  // The amount of health the player starts the game with.
+	public float currentHealth;                                   // The current health the player has.
 	public UnityEngine.UI.Slider healthSlider;                  // Reference to the UI's health bar.
 	public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
 	public AudioClip deathClip;                                 // The audio clip to play when the player dies.
